158b7dd72df7acf12327bb8835c9ceb57d0cf89b
luckarts <luc.bachelerieart@gmail.com>
2026-02-16 17:55:35 +0100

    chore(test): add AbstractApiTestCase base class

---
+++ backend/tests/E2E/AbstractApiTestCase.php
@@ -0,0 +1,18 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Tests\E2E;
+
+use App\Tests\E2E\Trait\ApiTestHelper;
+use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+
+abstract class AbstractApiTestCase extends WebTestCase
+{
+    use ApiTestHelper;
+
+    protected function setUp(): void
+    {
+        $this->client = static::createClient();
+    }
+}
