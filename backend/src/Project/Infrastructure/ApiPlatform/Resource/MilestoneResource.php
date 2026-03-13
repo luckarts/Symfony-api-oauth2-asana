d681a2720c80339e286e5307d92f53a520912487
luckarts <luc.bachelerieart@gmail.com>
2026-03-13 22:11:19 +0100

    feat(milestone): add GET endpoint with (collection + item)  + E2E tests

--- backend/src/Project/Infrastructure/ApiPlatform/Resource/MilestoneResource.php
+++ backend/src/Project/Infrastructure/ApiPlatform/Resource/MilestoneResource.php
@@ -5,24 +5,43 @@ declare(strict_types=1);
 namespace App\Project\Infrastructure\ApiPlatform\Resource;
 
 use ApiPlatform\Metadata\ApiResource;
+use ApiPlatform\Metadata\Get;
+use ApiPlatform\Metadata\GetCollection;
 use ApiPlatform\Metadata\Link;
 use ApiPlatform\Metadata\Post;
-use App\Project\Infrastructure\ApiPlatform\Resource\ProjectResource;
 use App\Project\Infrastructure\ApiPlatform\State\Processor\CreateMilestoneProcessor;
+use App\Project\Infrastructure\ApiPlatform\State\Provider\MilestoneCollectionProvider;
+use App\Project\Infrastructure\ApiPlatform\State\Provider\MilestoneItemProvider;
 use Symfony\Component\Validator\Constraints as Assert;
 
 #[ApiResource(
     shortName: 'Milestone',
     operations: [
-        new Post(
-            uriTemplate: '/projects/{id}/milestones',
+        new GetCollection(
+            uriTemplate: '/projects/{projectId}/milestones',
             uriVariables: [
-                'id' => new Link(fromClass: ProjectResource::class, identifiers: ['id']),
+                'projectId' => new Link(fromClass: self::class, identifiers: ['projectId']),
+            ],
+            provider: MilestoneCollectionProvider::class,
+            security: "is_granted('ROLE_USER')",
+        ),
+        new Post(
+            uriTemplate: '/projects/{projectId}/milestones',
+            uriVariables: [
+                'projectId' => new Link(fromClass: self::class, identifiers: ['projectId']),
             ],
             processor: CreateMilestoneProcessor::class,
             security: "is_granted('ROLE_USER')",
         ),
-        
+        new Get(
+            uriTemplate: '/projects/{projectId}/milestones/{id}',
+            uriVariables: [
+                'projectId' => new Link(fromClass: self::class, identifiers: ['projectId']),
+                'id' => new Link(fromClass: self::class, identifiers: ['id']),
+            ],
+            provider: MilestoneItemProvider::class,
+            security: "is_granted('ROLE_USER')",
+        ),
     ],
     routePrefix: '/api',
 )]
